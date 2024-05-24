a6ed877efb63e98edd77bae0cf0ded2b0354024e
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-05-24 22:50:33 +0530

    Time: 2 ms (74.91%) | Memory: 11.8 MB (98.22%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,7 +3,7 @@ public:
     int searchInsert(vector<int>& arr, int target) {
         int l=0;
         int r=arr.size()-1;
-        if(target>arr[r])return r+1;
+        // if(target>arr[r])return r+1;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(arr[mid]==target){
