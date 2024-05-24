68252399541cfaf22792d54bafc08522f4049e81
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-05-25 00:45:18 +0530

    Time: 3 ms (71.38%) | Memory: 12 MB (44.92%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int searchInsert(vector<int>& arr, int target) {
+        //so we have to calculate the upper bound of the value
         int l=0;
         int r=arr.size()-1;
-        // if(target>arr[r])return r+1;
+        int ans=-1;
+        if(target>arr[r])return r+1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            if(arr[mid]<target){
-                     l=mid+1;
-            }else{
-                r=mid-1;
-            }
+           if(arr[mid]>=target){
+            ans=mid;
+            r=mid-1;
+           }else{
+            l=mid+1;
+           }
         }
-        return l;
+        return ans;
     }
 };
