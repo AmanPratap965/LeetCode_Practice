9775f01dd8ddf1e9c1c800ce7543f13ade237c0d
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-05-27 18:07:44 +0530

    Time: 109 ms (73.15%) | Memory: 69 MB (27.47%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>&arr,int k,int mid){
+	int cnt=0;long long int total=0;
+	for(int i=0;i<arr.size();i++){
+        if(arr[i]<=mid)cnt++;
+        else {
+            total+=cnt/k;
+            cnt=0;
+        }
+    }
+    total+=cnt/k;
+	return  total;
+}
+    int minDays(vector<int>& arr, int m, int k) {
+        
+	int l=1;
+	int r=0;
+	for(auto it:arr)r=max(it,r);
+	if((long)k*m>arr.size())return -1;
+	while(l<=r){
+		int mid=l+(r-l)/2;
+		int total=solve(arr,k,mid);
+		 if(total>=m)r=mid-1;
+		else l=mid+1;
+	}
+	return l;
+    }
+};
