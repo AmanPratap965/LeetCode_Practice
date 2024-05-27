2a240f375af6722f9131b8ce57ef429183810c22
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-05-27 22:31:15 +0530

    Time: 45 ms (21.60%) | Memory: 33.4 MB (91.16%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int solve(int maxSum,vector<int>&weights){
+        int ans=0;
+        int sum=0;
+        for(auto it:weights){
+            if(it>maxSum)return -1;
+                if(sum+it<=maxSum)sum+=it;
+                else{
+                    ans++;
+                    sum=it;
+                }
+        }
+        if(sum<=maxSum)ans++;
+        return ans;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l=1;
+        int r=0;
+        for(int i=0;i<weights.size();i++)r+=weights[i];
+        int ans=0;
+        while(l<=r){
+                int mid=l+(r-l)/2;
+                int totalWeightDays=solve(mid,weights);
+                // if(totalWeightDays==days){
+                //     ans=mid;return ans;
+                // }
+                if(totalWeightDays==-1){
+                    l=mid+1;
+                    continue;
+                }
+                cout<<"mid "<<mid <<"totalDays "<<totalWeightDays<<endl;
+                if(totalWeightDays<=days){
+                    ans=mid;
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+                }
+        }
+        return ans;
+    }
+};
