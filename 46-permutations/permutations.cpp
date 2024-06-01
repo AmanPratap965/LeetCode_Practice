92e51f0414a7765c6ffe5e38d3e971625e46c84b
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-06-01 19:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (30.08%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>temp,vector<vector<int>>&ans,vector<int>&freq,int n){
-        if(temp.size()==n){
-            ans.push_back(temp);
+    void solve(int ind,vector<int>nums,vector<vector<int>>&ans,int n){
+        if(ind==n){
+            ans.push_back(nums);
             return;
         }
-        //selectin all the elements other then index
-        // freq[ind]=1;
-        for(int i=0;i<n;i++){
-            if(freq[i]==0){
-                temp.push_back(nums[i]);
-                freq[i]=1;
-                solve(nums,temp,ans,freq,n);
-                freq[i]=0;
-                temp.pop_back();
-            }
+        for(int i=ind;i<n;i++){
+            swap(nums[ind],nums[i]);
+            // temp.push_back(nums[ind]);
+            solve(ind+1,nums,ans,n);
+            // temp.pop_back();
+            swap(nums[ind],nums[i]);
         }
-        // freq[ind]=0;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>ans;
-        vector<int>temp;
-        //tracing the element with the help of freq array
-        vector<int>freq(n,0);
-        solve(nums,temp,ans,freq,n);
+        solve(0,nums,ans,n);
         return ans;
     }
 };
