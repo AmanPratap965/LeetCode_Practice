5ac09d37d04286721cdc23625ed315e5daafe62d
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-06-01 08:11:40 +0530

    Time: 4 ms (38.06%) | Memory: 8.4 MB (58.35%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>temp,int ind,int n){
-        if(ind==n){
-            ans.push_back(temp);
-            return;
-        }
-        //non-pick
-        solve(nums,ans,temp,ind+1,n);
-        //pick
-        temp.push_back(nums[ind]);
-        solve(nums,ans,temp,ind+1,n);
-        temp.pop_back();
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        solve(nums,ans,ds,0,nums.size());
+        int n=nums.size();
+        for(int i=0;i<(1<<n);i++){
+            vector<int>temp;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                        temp.push_back(nums[j]);
+                }
+            }
+            if(!temp.empty())
+            ans.push_back(temp);
+        }
+        ans.push_back({});
         return ans;
     }
 };
