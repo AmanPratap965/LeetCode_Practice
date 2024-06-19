de50d0ac6574d015318aa76ad0fa074e0bc5b939
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-06-19 10:35:37 +0530

    Time: 220 ms (34.15%) | Memory: 294.9 MB (5.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -15,17 +15,20 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         // return fn(0,-1,nums,n,dp);
         // for(int prevInd=0;prevInd<=n;prevInd++)dp[n][prevInd]=0;
+        vector<int>after(n+1,0);
+        vector<int>curr(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int prevInd=ind-1;prevInd>=-1;prevInd--){
                  int take=0;
                     
                     if(prevInd==-1 || nums[ind]>nums[prevInd]){
-                        take=1+dp[ind+1][ind+1];
+                        take=1+after[ind+1];
                     }
-                    int notTake=0+dp[ind+1][prevInd+1];
-                     dp[ind][prevInd+1]=max(take,notTake);
+                    int notTake=0+after[prevInd+1];
+                     curr[prevInd+1]=max(take,notTake);
             }
+            after=curr;
         }
-        return dp[0][0];
+        return after[0];
     }
 };
