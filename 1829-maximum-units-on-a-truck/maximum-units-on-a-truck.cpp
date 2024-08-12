d93b6ef82d8b51fa88f3e764bf6e2a262cc42620
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-08-12 18:43:12 +0530

    Time: 31 ms (41.49%) | Memory: 19.8 MB (48.99%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a,vector<int>&b){
+        return a[1]>b[1];
+    }
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),cmp);
+        int totalUnits=0;
+        int cnt=0;int i=0;
+        while(truckSize>0 && i<boxTypes.size()){
+            //if truckSize is greater than boxTypes[i][0]
+            if(truckSize>=boxTypes[i][0]){
+                totalUnits+=(boxTypes[i][0])*boxTypes[i][1];
+                truckSize-=boxTypes[i][0];
+                }
+
+            //if truckSize has become less than 0
+            else{
+                totalUnits+=truckSize*boxTypes[i][1];
+                truckSize=0;
+            }
+            i++;
+        }
+        return totalUnits;
+    }
+};
