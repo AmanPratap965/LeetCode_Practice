e7b9ce395e4f895525562eebb715aa761f82a536
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-08-12 22:29:05 +0530

    Time: 14 ms (6.77%) | Memory: 9.7 MB (9.30%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(int ind,string&s,int cnt,vector<vector<int>>&dp){
+    bool solve(int ind,string&s,int cnt,vector<vector<bool>>&dp){
         if(cnt<0)return false;
         if(ind==s.size())return dp[ind][cnt]=(cnt==0);
         if(dp[ind][cnt]!=-1)return dp[ind][cnt];
@@ -12,27 +12,25 @@ public:
         }
     }
     bool checkValidString(string s) {
-        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        return solve(0,s,0,dp);
-        // int ans;
-        // // stack<int>st;
-        // int open=0;
-        // int star=0;
-        // for(int i=0;i<s.size();i++){
-        //     if(s[i]=='(' )open=1;
-        //     else if(s[i]=='*')star=1;
-        //     else if(s[i]==')'){
-        //         if(open)open--;
-        //         else if(star)star--;
-        //         else return false;
-        //     }
-        // }
-        // while(open && star){
-        //     star--;
-        //     open--;
-        // }
-        // if(open)return false;
-        // return true;
+        vector<vector<bool>>dp(s.size()+1,vector<bool>(s.size()+1,0));
+        // return solve(0,s,0,dp);
+        int cnt=0;
+        int n=s.size();
+        //base case
+        for(int i=0;i<n;i++){
+            dp[n][i]=(i==0);
+        }
+            for(int ind=n-1;ind>=0;ind--){
+                for(int cnt=0;cnt<=n;cnt++){
+                    if(s[ind]=='(') dp[ind][cnt]=solve(ind+1,s,cnt+1,dp);
+                    else if(s[ind]==')') dp[ind][cnt]=solve(ind+1,s,cnt-1,dp);
+                    else {
+                        dp[ind][cnt]= (solve(ind+1,s,cnt-1,dp))|| (solve(ind+1,s,cnt,dp)) || (solve(ind+1,s,cnt+1,dp));
+                    }
+                }
+            }
+
+            return dp[0][0];
     }
 
 };
