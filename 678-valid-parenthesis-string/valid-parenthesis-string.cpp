48f014047beec85df029ec81338ee384445c6b7b
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-08-12 22:58:56 +0530

    Time: 9 ms (8.25%) | Memory: 9.1 MB (14.48%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(int ind,string&s,int cnt,vector<vector<bool>>&dp){
+    bool solve(int ind,string&s,int cnt,vector<vector<int>>&dp){
         if(cnt<0)return false;
         if(ind==s.size())return dp[ind][cnt]=(cnt==0);
         if(dp[ind][cnt]!=-1)return dp[ind][cnt];
@@ -12,24 +12,32 @@ public:
         }
     }
     bool checkValidString(string s) {
-        vector<vector<bool>>dp(s.size()+1,vector<bool>(s.size()+1,0));
-        // return solve(0,s,0,dp);
-        int cnt=0;
         int n=s.size();
-        //base case
-        for(int i=0;i<n;i++){
-            dp[n][i]=(i==0);
-        }
-            for(int ind=n-1;ind>=0;ind--){
-                for(int cnt=0;cnt<=n;cnt++){
-                    if(s[ind]=='(') dp[ind][cnt]=solve(ind+1,s,cnt+1,dp);
-                    else if(s[ind]==')') dp[ind][cnt]=solve(ind+1,s,cnt-1,dp);
-                    else {
-                        dp[ind][cnt]= (solve(ind+1,s,cnt-1,dp))|| (solve(ind+1,s,cnt,dp)) || (solve(ind+1,s,cnt+1,dp));
-                    }
-                }
-            }
+        //top-down approach -(memoization)
+        // vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
+        // return solve(0,s,0,dp);
 
+        //bottom-up approach
+        vector<vector<bool>>dp(n+1,vector<bool>(n+1,0));
+        //base cases
+        //when (ind==n) || other case will automatically handled by assigning zero to the dp
+        dp[n][0]=true;
+            // Bottom-up approach
+    for(int ind = n-1; ind >= 0; ind--) {
+        for(int cnt = 0; cnt <= n; cnt++) {
+            if(s[ind] == '(') {
+                if(cnt < n) {
+                    dp[ind][cnt] = dp[ind+1][cnt+1];
+                }
+            } else if(s[ind] == ')') {
+                if(cnt > 0) {
+                    dp[ind][cnt] = dp[ind+1][cnt-1];
+                }
+            } else {
+                dp[ind][cnt] = (cnt > 0 ? dp[ind+1][cnt-1] : false) || dp[ind+1][cnt] || (cnt < n ? dp[ind+1][cnt+1] : false);
+            }
+        }
+    }
             return dp[0][0];
     }
 
