bb8994bab4bf4a33c9962e6f27615534bc0a7308
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-08-23 09:29:38 +0530

    Time: 966 ms (9.84%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select id from weather w1 where temperature>(select temperature from weather w2 where w2.recordDate=DATE_SUB(w1.recordDate,interval 1 day));
+select id from weather w1 where temperature>(select temperature from weather w2 where w1.recordDate=DATE_ADD(w2.recordDate,interval 1 day));
