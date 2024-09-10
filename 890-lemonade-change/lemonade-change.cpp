4d5f4cb12e940c5e6a020ae801351f132354860b
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2024-09-10 10:46:16 +0530

    Time: 66 ms (78.69%) | Memory: 86.1 MB (52.78%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -5,16 +5,14 @@ public:
         for(int i=0;i<bills.size();i++){
             if(bills[i]==5)five++;
             else if(bills[i]==10){
-                if(!five)return false;
-                else{
-                    five--;
-                    ten++;
-                }
-            }else if(bills[i]==20){
+                ten++;
+                if(five)five--;
+                else return false;
+            }else{
                 if(five>=1 && ten>=1){
-                    ten--;
                     five--;
-                }else if(five>=3){five-=3;}
+                    ten--;
+                }else if(five>=3)five-=3;
                 else return false;
             }
         }
