3856a78ae69713e3df5996e1227c78733039ac7d
Aman Pratap Singh <128889001+AmanPratap965@users.noreply.github.com>
2025-04-07 00:12:06 +0530

    Time: 3 ms (32.22%) | Memory: 10.8 MB (6.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-   
-    void removeSpecialCharacter(string &s)
-{
-    for (int i = 0; i < s.size(); i++) {
- 
-        // Finding the character whose
-        // ASCII value fall under this
-        // range
-        if ((s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a'
-            || s[i] > 'z') && (s[i]<'0' || s[i]>'9')) {
-            // erase fundction to erase
-            // the character
-            s.erase(i, 1);
-            i--;
+    bool isPalindrome(string s) {
+        string final1="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a' && s[i]<='z' || (s[i]>='0' && s[i]<='9')){
+                final1+=s[i];
+            }else if(s[i]>='A' && s[i]<='Z'){
+                final1+=s[i]-'A'+'a';
+            }
         }
-    }
-   
-}
-void toLowerCase(string &str){
-    for(int i=0;i<str.size();i++){
-        if(str[i]>='A' && str[i]<='z')
-        str[i]=tolower(str[i]);
-    }
-    
-}
-    bool isPalindrome(string str) {
-        //removing all abnormal entries like white space, and non-alphanumeric characters
-        removeSpecialCharacter(str);
-        cout<<str;
-        toLowerCase(str);
-        int i=0;
-        int j=str.size()-1;
-        while(i<=j){
-            if(str[i]!=str[j])return false;
-            i++;
-            j--;
-        }
-        return true;
+        cout<<final1;
+        string a=final1;
+        //now check if the reverse is equal or not
+        reverse(final1.begin(),final1.end());
+        return a==final1?true:false;
     }
 };
